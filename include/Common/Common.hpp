d7d6898a923a4255d8dea7541de51d5f4b748583
cyanray <zyanglin@outlook.com>
2023-08-09 19:24:30 +0800

    code cleanup.

--- include/Common/Common.hpp
+++ include/Common/Common.hpp
@@ -7,20 +7,6 @@
 
 #include <tuple>
 
-#pragma comment(lib, "OccViewer.ViewerProxy.lib")
-#pragma comment(lib, "TKernel.lib")
-#pragma comment(lib, "TKMath.lib")
-#pragma comment(lib, "TKBRep.lib")
-#pragma comment(lib, "TKXSBase.lib")
-#pragma comment(lib, "TKService.lib")
-#pragma comment(lib, "TKV3d.lib")
-#pragma comment(lib, "TKOpenGl.lib")
-#pragma comment(lib, "TKD3dHost.lib")
-#pragma comment(lib, "TKLCAF.lib")
-#pragma comment(lib, "TKTopAlgo.lib")
-#pragma comment(lib, "TKG3d.lib")
-#pragma comment(lib, "TKGeomAlgo.lib")
-
 namespace OccEasyDrawing
 {
 	using AISObjects = NCollection_Vector<Handle(AIS_InteractiveObject)>;
